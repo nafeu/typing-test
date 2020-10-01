33d8c8f3e3363fe3b19e8bd5e0e1cd532a834c3b
Nafeu Nasir <nafeu.nasir@gmail.com>
2020-09-30 20:39:32 -0400

    Add base input fields and typing game logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./app.css";
 
 function App() {
-  return <div className="app">Hello World</div>;
+  const typingArea = useRef(null);
+
+  const [state, setState] = useState({
+    typingPrompt: "Hello World. This is my first typing game.",
+    completedCharacters: [],
+    isCorrectKeyPress: true,
+  });
+
+  const { typingPrompt, completedCharacters, isCorrectKeyPress } = state;
+
+  const handleOnKeyPress = ({ charCode }) => {
+    if (charCode === typingPrompt.charCodeAt(0)) {
+      setState({
+        completedCharacters: [...completedCharacters, typingPrompt[0]],
+        typingPrompt: typingPrompt.substring(1),
+        isCorrectKeyPress: true,
+      });
+    } else {
+      setState({
+        ...state,
+        isCorrectKeyPress: false,
+      });
+    }
+  };
+
+  const handleClick = () => {
+    typingArea.current.focus();
+  };
+
+  return (
+    <div onClick={handleClick} className="app">
+      <input
+        ref={typingArea}
+        className="typing-area"
+        onKeyPress={handleOnKeyPress}
+        type="text"
+        autoFocus
+      />
+      <div>
+        {completedCharacters.map((completedCharacter, index) => {
+          return (
+            <span
+              key={`${completedCharacter}-${index}`}
+              className={
+                isCorrectKeyPress ? "correct-character" : "incorrect-character"
+              }
+            >
+              {completedCharacter}
+            </span>
+          );
+        })}
+        {typingPrompt}
+      </div>
+    </div>
+  );
 }
 
 export default App;
